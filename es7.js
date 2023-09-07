13ba2fe231cc8a7b16555e2cbba5f928ca993663
enzosamojedny <76167611+enzosamojedny@users.noreply.github.com>
2023-09-07 10:45:32 -0300

    Add files via upload

--- es7.js
+++ es7.js
@@ -1,12 +1,10 @@
-class ticketManager {
+class TicketManager {
   #precioBaseDeGanancia = 0.15;
+  eventos = []; // Moved the array initialization outside the constructor
 
-  constructor() {
-    this.eventos = [];
-  }
+  constructor() {}
 
   getEventos() {
-    //method
     return this.eventos;
   }
 
@@ -25,5 +23,41 @@ class ticketManager {
       fecha,
       participantes: [],
     };
+    if (this.eventos.length === 0) {
+      evento.id = 1;
+    } else {
+      evento.id = this.eventos[this.eventos.length - 1].id + 1;
+    }
+    this.eventos.push(evento);
+  };
+
+  agregarParticipantes = (idEvento, idParticipante) => {
+    const eventoIndex = this.eventos.findIndex(
+      (evento) => evento.id === idEvento
+    );
+    if (eventoIndex === -1) {
+      console.log("Evento no encontrado");
+      return;
+    }
+    const usuarioRegistrado = this.eventos[eventoIndex].participantes.includes(
+      idParticipante
+    );
+    if (usuarioRegistrado) {
+      console.log("Usuario ya está registrado");
+      return;
+    }
+    this.eventos[eventoIndex].participantes.push(idParticipante);
   };
 }
+
+const manejadorDeEventos = new TicketManager();
+
+manejadorDeEventos.agregarEvento("Evento 1", "Perú", 200, 50000);
+manejadorDeEventos.agregarEvento("Evento 3", "Argentina", 200);
+manejadorDeEventos.agregarEvento("Evento 2", "Brasil", 500, 100000);
+manejadorDeEventos.agregarParticipantes(1, 1234);
+manejadorDeEventos.agregarParticipantes(2, 1234);
+manejadorDeEventos.agregarParticipantes(3, 1234);
+manejadorDeEventos.agregarParticipantes(1, 1234); // Adding a participant to Evento 1 again
+
+console.log(manejadorDeEventos.getEventos());
